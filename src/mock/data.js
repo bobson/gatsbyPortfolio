8fce6df411050db49f18042c2a05323d51489a0a
SlobodanMarkoski <bmarkoski@yahoo.com>
2023-05-22 12:33:42 +0200

    Add a project

--- src/mock/data.js
+++ src/mock/data.js
@@ -32,8 +32,7 @@ export const projectsData = [
     id: nanoid(),
     img: "CRWNClothing.png",
     title: "CRWN Clothing",
-    info:
-      "E-commece web page build with React usig Redux for state managment routing with react-router, firebase for user storage, and stripe for payment, styled with scss.",
+    info: "E-commece web page build with React usig Redux for state managment routing with react-router, firebase for user storage, and stripe for payment, styled with scss.",
     info2: "",
     url: "https://crwn-shop-react.netlify.app/",
     repo: "https://github.com/bobson/crwn-shop.git", // if no repo, the button will not show up
@@ -43,8 +42,7 @@ export const projectsData = [
     id: nanoid(),
     img: "AmazonClone.png",
     title: "Amazon Clone",
-    info:
-      "Amazon website with FULL E-Commerce Functionality build with React, firebase, stripe.",
+    info: "Amazon website with FULL E-Commerce Functionality build with React, firebase, stripe.",
     info2: "",
     url: "https://clone-6f1af.web.app/",
     repo: "https://github.com/bobson/amazon-clone", // if no repo, the button will not show up
@@ -52,10 +50,20 @@ export const projectsData = [
 
   {
     id: nanoid(),
+    img: "Sushiman.png",
+    title: "Sushiman Webpage",
+    info: "A Responsive Website build with HTML and CSS ",
+    info2:
+      "Built with Semantic HTML5 markup, CSS custom properties, Flexbox, Resposive Mobile workflow, Vite - JS library",
+    url: "https://sushiman-web.netlify.app/",
+    repo: "https://github.com/bobson/sushiman", // if no repo, the button will not show u
+  },
+
+  {
+    id: nanoid(),
     img: "BrainsterSpace.png",
     title: "Brainster Space",
-    info:
-      "Final Project of the Braister bootcamp build with React and Bootsrap",
+    info: "Final Project of the Braister bootcamp build with React and Bootsrap",
     info2: "",
     url: "https://brainsterspace-slobodan.netlify.app",
     repo: "https://github.com/bobson/BrainsterSpace.git", // if no repo, the button will not show u
@@ -76,8 +84,7 @@ export const projectsData = [
     id: nanoid(),
     img: "virtualBanking.png",
     title: "Virtual Banking",
-    info:
-      "Banking landing page build with Next.js and styled components, and react scroll for smoot scroll animations ",
+    info: "Banking landing page build with Next.js and styled components, and react scroll for smoot scroll animations ",
     info2: "",
     url: "https://next-virtula-banking-3yup2h0ni-bobson.vercel.app/",
     repo: "https://github.com/bobson/next-virtula-banking", // if no repo, the button will not show up
@@ -87,8 +94,7 @@ export const projectsData = [
     id: nanoid(),
     img: "E-commerce.png",
     title: "E-commerce",
-    info:
-      "This is a web shop build with react and commercejs.com, styled with material-UI. I am usig lates features on React and commercejs as a database for the products. Clean, fast, user friendly e-commerce page.",
+    info: "This is a web shop build with react and commercejs.com, styled with material-UI. I am usig lates features on React and commercejs as a database for the products. Clean, fast, user friendly e-commerce page.",
     info2:
       "You can add as many products on the  commercejs.com and use it as a backend with wery simple API request for the products. Payment proccess with stripe",
     url: "https://re-commercejs.netlify.app",
@@ -98,22 +104,10 @@ export const projectsData = [
     id: nanoid(),
     img: "Covid-19.png",
     title: "Covid-19 Tracker",
-    info:
-      "This project is a simple COVID-19 Virus tracker app build with React.js with all the latest features like hooks, styled with Materilal UI, visualization with Charts.js",
+    info: "This project is a simple COVID-19 Virus tracker app build with React.js with all the latest features like hooks, styled with Materilal UI, visualization with Charts.js",
     info2: "",
     url: "https://covidvirustracker.netlify.app",
     repo: "https://github.com/bobson/covid_19_tracker", // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: "AdoptMe.png",
-    title: "Adopt Me",
-    info:
-      "Pet adoption site where you can adopt various pets, from frontendmasters api call.",
-    info2:
-      "Build with react, parcel as bundler, it has 90% of React features, hooks, context, portal, reatch/router errorBounadry.",
-    url: "https://pet-adopt.netlify.app",
-    repo: "https://github.com/bobson/pet-adopt.git", // if no repo, the button will not show up
   },
 ];
 
