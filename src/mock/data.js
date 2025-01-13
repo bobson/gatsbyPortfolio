1902f76e0cefe28a9c15344a599a9508ad41e7ee
Slobodan <bmarkoski@gmail.com>
2025-01-13 14:42:43 +0100

    Add a project

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne:
     "Ambitious and motivated individual, proficient in both clean design and most bleeding-edge web technologies. I possess self-discipline, ability to work with a minimum of supervision and strive to perform my duties to ensure maximum accessibility.",
   paragraphTwo:
-    "Results-oriented Front-end Developer love using React and Material UI, leverage analytical skills and strong attention to detail in order to deliver original and efficient web solutions, provide technical knowledge, build new websites from start to finish.",
+    "Results-oriented Front-end Developer using React but also other frameworks, leverage analytical skills and strong attention to detail in order to deliver original and efficient web solutions, provide technical knowledge, build new websites from start to finish.",
   paragraphThree: " ",
   resume: "https://www.resumemaker.online/es.php", // if no resume, the button will not show up
 };
@@ -30,12 +30,12 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: "CRWNClothing.png",
-    title: "CRWN Clothing",
-    info: "E-commece web page build with React usig Redux for state managment routing with react-router, firebase for user storage, and stripe for payment, styled with scss.",
+    img: "php_react_full.png",
+    title: "Full stack PHP and React",
+    info: "I built a web app with a PHP backend using OOP principles, React with TypeScript for a scalable frontend, and TailwindCSS for responsive design. Docker is used for containerization, ensuring consistent environments across development and production.",
     info2: "",
-    url: "https://crwn-shop-react.netlify.app/",
-    repo: "https://github.com/bobson/crwn-shop.git", // if no repo, the button will not show up
+    url: "https://lively-elf-21a729.netlify.app/all",
+    repo: "https://bitbucket.org/slobodan3/scandi_frontend/src/master/", // if no repo, the button will not show up
   },
 
   {
@@ -69,16 +69,15 @@ export const projectsData = [
     repo: "https://github.com/bobson/BrainsterSpace.git", // if no repo, the button will not show u
   },
 
-  // {
-  //   id: nanoid(),
-  //   img: "Carousel.png",
-  //   title: "Responsive swipe Carousel ",
-  //   info:
-  //     "Responsie finger-folowing swipe Carousel component with infinite loop option, supports multiple slides on the screen, Supports scrolling to a selected slide,work for any HTML content, autoplay option.",
-  //   info2: "",
-  //   url: "https://carousel-finger-folow.netlify.app/",
-  //   repo: "https://github.com/bobson/Carousel", // if no repo, the button will not show up
-  // },
+  {
+    id: nanoid(),
+    img: "Carousel.png",
+    title: "Responsive swipe Carousel ",
+    info: "Responsie finger-folowing swipe Carousel component with infinite loop option, supports multiple slides on the screen, Supports scrolling to a selected slide,work for any HTML content, autoplay option.",
+    info2: "",
+    url: "https://carousel-finger-folow.netlify.app/",
+    repo: "https://github.com/bobson/Carousel", // if no repo, the button will not show up
+  },
 
   {
     id: nanoid(),
@@ -90,25 +89,25 @@ export const projectsData = [
     repo: "https://github.com/bobson/next-virtula-banking", // if no repo, the button will not show up
   },
 
-  {
-    id: nanoid(),
-    img: "E-commerce.png",
-    title: "E-commerce",
-    info: "This is a web shop build with react and commercejs.com, styled with material-UI. I am usig lates features on React and commercejs as a database for the products. Clean, fast, user friendly e-commerce page.",
-    info2:
-      "You can add as many products on the  commercejs.com and use it as a backend with wery simple API request for the products. Payment proccess with stripe",
-    url: "https://re-commercejs.netlify.app",
-    repo: "https://github.com/bobson/react-e-shop", // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: "Covid-19.png",
-    title: "Covid-19 Tracker",
-    info: "This project is a simple COVID-19 Virus tracker app build with React.js with all the latest features like hooks, styled with Materilal UI, visualization with Charts.js",
-    info2: "",
-    url: "https://covidvirustracker.netlify.app",
-    repo: "https://github.com/bobson/covid_19_tracker", // if no repo, the button will not show up
-  },
+  // {
+  //   id: nanoid(),
+  //   img: "E-commerce.png",
+  //   title: "E-commerce",
+  //   info: "This is a web shop build with react and commercejs.com, styled with material-UI. I am usig lates features on React and commercejs as a database for the products. Clean, fast, user friendly e-commerce page.",
+  //   info2:
+  //     "You can add as many products on the  commercejs.com and use it as a backend with wery simple API request for the products. Payment proccess with stripe",
+  //   url: "https://re-commercejs.netlify.app",
+  //   repo: "https://github.com/bobson/react-e-shop", // if no repo, the button will not show up
+  // },
+  // {
+  //   id: nanoid(),
+  //   img: "Covid-19.png",
+  //   title: "Covid-19 Tracker",
+  //   info: "This project is a simple COVID-19 Virus tracker app build with React.js with all the latest features like hooks, styled with Materilal UI, visualization with Charts.js",
+  //   info2: "",
+  //   url: "https://covidvirustracker.netlify.app",
+  //   repo: "https://github.com/bobson/covid_19_tracker", // if no repo, the button will not show up
+  // },
 ];
 
 // CONTACT DATA
